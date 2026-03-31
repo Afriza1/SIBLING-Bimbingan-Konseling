a035089e075c62c3811d9533683816d8c0cf2d04
Afrizatif <144039008+Afriza1@users.noreply.github.com>
2026-03-31 07:35:53 +0700

    feat(model): add Lateness model

---
+++ app/Models/lateness.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lateness extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'user_id',
+        'date',
+        'time_in',
+        'reason',
+        'evidence',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
